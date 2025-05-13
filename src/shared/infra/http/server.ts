7c817ab6c69e08ddae42a5e7b8a35e95bb7ee1f6
JoaoBorchoski <joaoborchoskidev@gmail.com>
2025-05-12 22:14:39 -0300

    First deploy

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,9 @@
-import { app } from './app'
-import { mqWorkerRun } from '@shared/infra/mq/mq-worker-run'
+import { app } from "./app"
+import { mqWorkerRun } from "@shared/infra/mq/mq-worker-run"
+import "dotenv/config"
 
-app.listen(3333, () => console.log('Server is running!'))
+const port = process.env.PORT ? Number(process.env.PORT) : 3333
+
+app.listen(port, () => console.log(`Server is running on port ${port}!`))
 
 mqWorkerRun()
